7587d6cca2cfce194c567d4fce56608f2bc2edc1
sakayat <sakhyatkabir1@gmail.com>
2024-08-12 21:54:41 +0600

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../assets/images/footer_logo.svg";
 import { Link, NavLink } from "react-router-dom";
-import { Facebook, Twitter } from "lucide-react";
+import { Facebook, HomeIcon, MailIcon, PhoneCall, Twitter } from "lucide-react";
 
 const Footer = () => {
   return (
@@ -12,7 +12,7 @@ const Footer = () => {
             <NavLink to="/">
               <img src={logo} alt="" className="w-72 h-full" />
             </NavLink>
-            <div className="grid grid-cols-12 gap-5 mb-10">
+            <div className="grid grid-cols-12 gap-10 lg:gap-5 mb-10">
               <div className="col-span-12 lg:col-span-5 space-y-5">
                 <h3 className="text-2xl">About Us</h3>
                 <p>
@@ -20,6 +20,26 @@ const Footer = () => {
                   are working all over the world, organizing blood donation
                   campaign to grow awareness among the people to donate blood.
                 </p>
+                <div className="contact-info space-y-5">
+                  <div className="flex gap-3">
+                    <span>
+                      <HomeIcon />
+                    </span>
+                    <span>23/1, Taltala, Sylhet-3100, Bangladesh</span>
+                  </div>
+                  <div className="flex gap-3">
+                    <span>
+                      <PhoneCall />
+                    </span>
+                    <span>+8801759-231892</span>
+                  </div>
+                  <div className="flex gap-3">
+                    <span>
+                      <MailIcon />
+                    </span>
+                    <span>safe.donor@gmail.com</span>
+                  </div>
+                </div>
               </div>
               <div className="col-span-12 lg:col-span-3 space-y-5">
                 <h3 className="text-2xl">Quick Links</h3>
