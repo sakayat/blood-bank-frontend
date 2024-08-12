26b8c9b0dfc98a4054e55c81b4184681a0c7af97
sakayat <sakhyatkabir1@gmail.com>
2024-08-13 00:21:43 +0600

    updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -15,13 +15,16 @@ const Login = () => {
       username: username,
       password: password,
     };
-    const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/accounts/login/`, {
-      method: "post",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(userObj),
-    });
+    const res = await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/api/accounts/login/`,
+      {
+        method: "post",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(userObj),
+      }
+    );
 
     const data = await res.json();
 
@@ -31,57 +34,53 @@ const Login = () => {
     }
     setError(data);
   };
-  const url = window.location.search.replace("?", "")
-  const decodeMsg = decodeURIComponent(url)
-  
+  const url = window.location.search.replace("?", "");
+  const decodeMsg = decodeURIComponent(url);
+
   return (
     <section className="login pt-10 mt-20">
       <div className="container mx-auto px-4">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 place-items-center">
-            <div className="lg:order-last">
-              <img src={img} alt="" className="w-full h-full" />
-            </div>
-            <form
-              className="py-5 px-8 space-y-3 w-full"
-              onSubmit={handleSubmit}
-            >
-              <div className="py-3">
-                <h1 className="text-2xl font-bold">Login</h1>
-                {url && <p className="pt-3 text-rose-500">{decodeMsg}</p>}
-              </div>
-              <div className="form-control space-y-3">
-                <label htmlFor="username">Username</label>
-                <input
-                  type="text"
-                  id="username"
-                  placeholder="username"
-                  className="w-full outline-none py-3 px-4 border border-black/20 bg-white rounded"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-              </div>
-
-              <div className="form-control space-y-3">
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  placeholder="Password"
-                  className="w-full outline-none py-3 px-4 border border-black/20 bg-white rounded"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <button className="default-btn py-3 w-full rounded">
-                Submit
-              </button>
-              {error && (
-                <p className="py-3 text-rose-500">
-                  {error.error ? error.error : error.username}
-                </p>
-              )}
-
-            </form>
+        <div className="py-3">
+          <h1 className="text-2xl font-bold text-center">Login</h1>
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 place-items-center">
+          <div className="lg:order-last">
+            <img src={img} alt="" className="w-full h-full" />
           </div>
+          <form className="py-5 px-8 space-y-3 w-full" onSubmit={handleSubmit}>
+            <div className="py-3">
+              {url && <p className="pt-3 text-rose-500">{decodeMsg}</p>}
+            </div>
+            <div className="form-control space-y-3">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                placeholder="username"
+                className="w-full outline-none py-3 px-4 border border-black/20 bg-white rounded"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+
+            <div className="form-control space-y-3">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                placeholder="Password"
+                className="w-full outline-none py-3 px-4 border border-black/20 bg-white rounded"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button className="default-btn py-3 w-full rounded">Submit</button>
+            {error && (
+              <p className="py-3 text-rose-500">
+                {error.error ? error.error : error.username}
+              </p>
+            )}
+          </form>
+        </div>
       </div>
     </section>
   );
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -42,71 +42,68 @@ const SignUp = () => {
   return (
     <section className="registration pt-10 mt-20">
       <div className="container mx-auto px-4">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 place-items-center">
-            <div className="">
-              <img src={img} alt="" className="w-full h-full" />
-            </div>
-            <form
-              className="py-5 px-8 space-y-3 w-full"
-              onSubmit={handleSubmit}
-            >
-              <div className="py-3">
-                <h1 className="text-2xl">Create a new account</h1>
-              </div>
-              <div className="form-control space-y-3">
-                <label htmlFor="username">Username</label>
-                <input
-                  type="text"
-                  id="username"
-                  placeholder="username"
-                  className="w-full outline-none py-3 px-4 border border-black/20 bg-white rounded"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-              </div>
-              <div className="form-control space-y-3">
-                <label htmlFor="email">Email</label>
-                <input
-                  type="email"
-                  id="email"
-                  placeholder="Email"
-                  className="w-full outline-none py-3 px-4 border border-black/20 bg-white rounded"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-              <div className="form-control space-y-3">
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  placeholder="Password"
-                  className="w-full outline-none py-3 px-4 border border-black/20 bg-white rounded"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-
-              <div className="form-control space-y-3">
-                <label htmlFor="confirm-password">Confirm Password</label>
-                <input
-                  type="password"
-                  id="confirm-password"
-                  placeholder="Confirm Password"
-                  className="w-full outline-none py-3 px-4 border border-black/20 bg-white rounded"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-              </div>
-              <button className={`default-btn py-3 w-full rounded`}>
-                Submit
-              </button>
-              {error && (
-                <p className="py-3 text-rose-500">
-                  {error.error ? error.error : error.username}
-                </p>
-              )}
-            </form>
+        <div className="py-3">
+          <h1 className="text-2xl font-bold text-center">Create a new account</h1>
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 place-items-center">
+          <div className="">
+            <img src={img} alt="" className="w-full h-full" />
           </div>
+          <form className="py-5 px-8 space-y-3 w-full" onSubmit={handleSubmit}>
+            <div className="form-control space-y-3">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                placeholder="username"
+                className="w-full outline-none py-3 px-4 border border-black/20 bg-white rounded"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="form-control space-y-3">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Email"
+                className="w-full outline-none py-3 px-4 border border-black/20 bg-white rounded"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="form-control space-y-3">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                placeholder="Password"
+                className="w-full outline-none py-3 px-4 border border-black/20 bg-white rounded"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <div className="form-control space-y-3">
+              <label htmlFor="confirm-password">Confirm Password</label>
+              <input
+                type="password"
+                id="confirm-password"
+                placeholder="Confirm Password"
+                className="w-full outline-none py-3 px-4 border border-black/20 bg-white rounded"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+            <button className={`default-btn py-3 w-full rounded`}>
+              Submit
+            </button>
+            {error && (
+              <p className="py-3 text-rose-500">
+                {error.error ? error.error : error.username}
+              </p>
+            )}
+          </form>
+        </div>
       </div>
     </section>
   );
