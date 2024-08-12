2a8055432dc43ce7ea081a31bd35ccb478ae2ce4
sakayat <sakhyatkabir1@gmail.com>
2024-08-12 21:05:34 +0600

    updated contact form and location

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import headerImg from "../assets/images/section_title.jpg";
 import { HomeIcon, MailIcon, PhoneCall } from "lucide-react";
+import emailjs from "@emailjs/browser";
 
 const ContactUs = () => {
+  const form = useRef();
+
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await emailjs.sendForm(
+      "service_s1u5wm3",
+      "template_5rf2qb8",
+      form.current,
+      {
+        publicKey: "U4HUSy-SQ03bRZ9Za",
+      }
+    );
+
+    if (res.status) {
+      setMessage("Message send successfully");
+      e.target.reset();
+    }
+  };
+
   return (
     <section className="pt-10 mt-10">
       <div className="relative h-64">
@@ -33,6 +56,54 @@ const ContactUs = () => {
               <MailIcon />
             </span>
             <span>safe.donor@gmail.com</span>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
+          <div className="contact-form">
+            <h2 className="text-2xl font-semibold">Contact</h2>
+            <form
+              className="py-5  space-y-3 w-full"
+              ref={form}
+              onSubmit={handleSubmit}
+            >
+              <input
+                type="text"
+                name="user_name"
+                placeholder="Name"
+                className="w-full outline-none py-3 px-4 border border-black/20 bg-white rounded"
+                required
+              />
+              <input
+                type="text"
+                name="user_email"
+                placeholder="Email"
+                className="w-full outline-none py-3 px-4 border border-black/20 bg-white rounded"
+                required
+              />
+              <textarea
+                type="text"
+                name="message"
+                placeholder="Message"
+                className="w-full outline-none py-3 px-4 border border-black/20 bg-white rounded"
+                rows={6}
+                required
+              />
+              <button className="default-btn py-3 w-full">Submit</button>
+              <p className="text-rose-400 py-3">{message}</p>
+            </form>
+          </div>
+          <div className="location">
+            <h2 className="text-2xl font-semibold">Location</h2>
+            <div className="py-5 w-full">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3619.196948395475!2d91.86309617502097!3d24.891262844025054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3751abe61a59105d%3A0x75be7d94ceb0fbc3!2s23%2C%201%20Taltola%20Rd%2C%20Sylhet%203100!5e0!3m2!1sen!2sbd!4v1723474616941!5m2!1sen!2sbd"
+                width="600"
+                height="400"
+                style={{ border: 0 }}
+                loading="lazy"
+                className="w-full"
+              ></iframe>
+            </div>
           </div>
         </div>
       </div>
